0ddd429a9a3d072e43d990a917645a88434b526a
petershine <petershine@fxceed.com>
2012-11-22 01:49:12 +0900

    REFACTORED to use super class more

--- Views/FXDPopoverBackgroundView.h
+++ Views/FXDPopoverBackgroundView.h
@@ -6,6 +6,45 @@
 //  Copyright (c) 2012 Ensight. All rights reserved.
 //
 
+#ifndef popoverCornerRadius
+	#define popoverCornerRadius 8.0
+#endif
+
+#ifndef popoverArrowOffset
+	#define popoverArrowOffset ￼0
+#endif
+
+#ifndef popoverArrowDirection
+	#define popoverArrowDirection UIPopoverArrowDirectionUp
+#endif
+
+
+#ifndef popoverArrowHeight
+	#define popoverArrowHeight ￼0
+#endif
+
+#ifndef popoverArrowBase
+	#define popoverArrowBase ￼0
+#endif
+
+
+#ifndef popoverContentViewInsetsTop
+	#define popoverContentViewInsetsTop		10.0
+#endif
+
+#ifndef popoverContentViewInsetsLeft
+	#define popoverContentViewInsetsLeft	10.0
+#endif
+
+#ifndef popoverContentViewInsetsBottom
+	#define popoverContentViewInsetsBottom	10.0
+#endif
+
+#ifndef popoverContentViewInsetsRight
+	#define popoverContentViewInsetsRight	10.0
+#endif
+
+
 #import "FXDKit.h"
 
 
@@ -15,19 +54,23 @@
 	UIPopoverArrowDirection _arrowDirection;
 
 	// Instance variables
-
 }
 
 // Properties
+@property (strong, nonatomic) NSString *titleText;
 
 // IBOutlets
 @property (strong, nonatomic) IBOutlet UIView *backgroundView;
+@property (strong, nonatomic) IBOutlet UIView *titleView;
 
 
 #pragma mark - IBActions
 
 
 #pragma mark - Public
++ (FXDPopoverBackgroundView*)sharedInstance;
+
+- (void)configureTitleTextAndTitleViewFromSharedInstance;
 
 
 //MARK: - Observer implementation
--- Views/FXDPopoverBackgroundView.m
+++ Views/FXDPopoverBackgroundView.m
@@ -14,6 +14,14 @@
 
 
 #pragma mark - Memory management
+- (void)dealloc {	FXDLog_OVERRIDE;
+	[[self class] sharedInstance].titleText = nil;
+	[[self class] sharedInstance].titleView = nil;
+	
+	// Instance variables
+
+	// Properties
+}
 
 
 #pragma mark - Initialization
@@ -22,10 +30,10 @@
 
     if (self) {//MARK: Cannot use awakeFromNib
 #if DEBUG
-		self.backgroundColor = [UIColor darkGrayColor];
+		self.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.5];
 #endif
 		self.layer.masksToBounds = YES;
-		self.layer.cornerRadius = 8;
+		self.layer.cornerRadius = popoverCornerRadius;
 
 		self.layer.shadowColor = [UIColor blackColor].CGColor;
 		self.layer.shadowOpacity = 0.4;
@@ -33,7 +41,6 @@
 		self.layer.shadowOffset = CGSizeMake(0, 2);
 
 		// Primitives
-		_arrowDirection = UIPopoverArrowDirectionUp;
 
 		// Instance variables
 
@@ -44,6 +51,10 @@
     }
 
     return self;
+}
+
++ (FXDPopoverBackgroundView*)sharedInstance {
+	IMPLEMENTATION_sharedInstance;
 }
 
 
@@ -66,29 +77,78 @@
 
 
 #pragma mark - Method overriding
-+ (CGFloat)arrowHeight {	FXDLog_OVERRIDE;
-	return 0;
++ (CGFloat)arrowHeight {
+	return popoverArrowHeight;
 }
 
-+ (CGFloat)arrowBase {	FXDLog_OVERRIDE;
-	return 0;
++ (CGFloat)arrowBase {
+	return popoverArrowBase;
 }
 
-+ (UIEdgeInsets)contentViewInsets {	FXDLog_OVERRIDE;
-	return UIEdgeInsetsMake(10, 10, 10, 10);
++ (UIEdgeInsets)contentViewInsets {
+	return UIEdgeInsetsMake(popoverContentViewInsetsTop, popoverContentViewInsetsLeft, popoverContentViewInsetsBottom, popoverContentViewInsetsRight);
 }
 
 - (void)layoutSubviews {	FXDLog_DEFAULT;
 	[super layoutSubviews];
 
-	FXDLog(@"self.frame: %@", NSStringFromCGRect(self.frame));
-	FXDLog(@"1.self.backgroundView: %@", self.backgroundView);
+	if (self.backgroundView == nil) {
+		CGRect modifiedFrame = self.frame;
+		modifiedFrame.origin.x = 0.0;
+		modifiedFrame.origin.y = popoverArrowHeight;
+		modifiedFrame.size.height -= popoverArrowHeight;
+
+		self.backgroundView = [[UIView alloc] initWithFrame:modifiedFrame];
+		self.backgroundView.backgroundColor = [UIColor blackColor];
+
+		self.backgroundView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+
+		self.backgroundView.layer.masksToBounds = YES;
+		self.backgroundView.layer.cornerRadius = popoverCornerRadius;
+
+		[self addSubview:self.backgroundView];
+		[self sendSubviewToBack:self.backgroundView];
+	}
+	
+
+	[self configureTitleTextAndTitleViewFromSharedInstance];
+
+	if (self.titleView == nil && self.titleText) {
+		self.titleView = [[UILabel alloc] initWithFrame:
+						  CGRectMake(0, 0, self.frame.size.width, popoverContentViewInsetsTop)];
+
+		self.titleView.backgroundColor = [UIColor clearColor];
+
+		[(UILabel*)self.titleView setTextAlignment:NSTextAlignmentCenter];
+		[(UILabel*)self.titleView setFont:[UIFont boldSystemFontOfSize:20]];
+		[(UILabel*)self.titleView setTextColor:[UIColor whiteColor]];
+		[(UILabel*)self.titleView setShadowColor:[UIColor darkGrayColor]];
+
+		[(UILabel*)self.titleView setText:self.titleText];
+	}
+	
+	if (self.titleView) {
+		CGRect modifiedFrame = self.titleView.frame;
+		modifiedFrame.origin.x = ((self.frame.size.width -modifiedFrame.size.width)/2.0);
+		modifiedFrame.origin.y = ((popoverContentViewInsetsTop -modifiedFrame.size.height)/2.0);
+		modifiedFrame.origin.y += popoverArrowHeight;
+		[self.titleView setFrame:modifiedFrame];
+
+		[self addSubview:self.titleView];
+		[self bringSubviewToFront:self.titleView];
+	}
 }
+
 
 #pragma mark - IBActions
 
 
 #pragma mark - Public
+- (void)configureTitleTextAndTitleViewFromSharedInstance {	FXDLog_OVERRIDE;
+
+	self.titleText = [[self class] sharedInstance].titleText;
+	self.titleView = [[self class] sharedInstance].titleView;
+}
 
 
 //MARK: - Observer implementation
