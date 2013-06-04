790627f79204e6e5245e376baf5d2725d2b598fd
petershine <petershine@fxceed.com>
2013-06-04 13:47:20 +0900

    REFACTORED logging

--- __headers/FXDconfigDeveloper.h
+++ __headers/FXDconfigDeveloper.h
@@ -34,16 +34,12 @@
 	#define FXDLog NSLog
 
 	#define FXDLog_DEFAULT	FXDLog(@" ");FXDLog(@"%@", strClassSelector)
+	#define FXDLog_FRAME	FXDLog(@" ");FXDLog(@"%@: %@", strClassSelector, NSStringFromCGRect(self.view.frame))
 
 	#define FXDLog_SEPARATE			FXDLog(@"\n\n__  %@  __", strClassSelector)
 	#define FXDLog_SEPARATE_FRAME	FXDLog(@"\n\n__  %@: %@ __", strClassSelector, NSStringFromCGRect(self.view.frame))
 
-	#define FXDLog_VIEW_FRAME	FXDLog(@" ");FXDLog(@"%@: %@", strClassSelector, NSStringFromCGRect(self.frame));
-
 	#define FXDLog_OVERRIDE	FXDLog(@" ");FXDLog(@"OVERRIDE: %@", strClassSelector)
-
-	#define FXDLog_NOTIFIED	FXDLog(@" ");FXDLog(@"NOTIFIED: %@: %@", NSStringFromClass([self class]), note.name)
-
 
 	#define FXDLog_ERROR	if(error){FXDLog(@" ");FXDLog(@"\n\n%@\nfile: %s\nline: %d\n\nlocalizedDescription: %@\ndomain: %@\ncode: %d\nuserInfo:\n%@\n\n", strClassSelector, __FILE__, __LINE__, [error localizedDescription], [error domain], [error code], [error userInfo]);}
 
@@ -54,15 +50,12 @@
 	#define FXDLog(format, ...)	{}
 
 	#define FXDLog_DEFAULT
+	#define FXDLog_FRAME
 
 	#define FXDLog_SEPARATE
 	#define FXDLog_SEPARATE_FRAME
 
-	#define FXDLog_VIEW_FRAME
-
 	#define FXDLog_OVERRIDE
-
-	#define FXDLog_NOTIFIED
 
 	#define FXDLog_ERROR
 	#define FXDLog_ERRORexcept(v)
