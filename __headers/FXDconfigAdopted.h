f712f8f45ac4126c5e82d42be07e27a413eecdd5
petershine <petershine@fxceed.com>
2014-02-28 17:40:31 +0900

    CHANGED conditions

--- __headers/FXDconfigAdopted.h
+++ __headers/FXDconfigAdopted.h
@@ -10,16 +10,24 @@
 #define FXDKit_FXDconfigAdopted_h
 
 
-#ifndef USE_ExtraFrameworks
-	#define USE_ExtraFrameworks	1
-#endif
-
 #ifndef USE_AFNetworking
 	#define USE_AFNetworking	1
 #endif
 
+
+#ifndef USE_ExtraFrameworks
+	#define USE_ExtraFrameworks	0
+#endif
+
+
 #ifndef USE_UAAppReviewManager
-	#define	USE_UAAppReviewManager	1
+	#define	USE_UAAppReviewManager	0
+#endif
+
+
+#if USE_AFNetworking
+	#import <AFNetworking.h>
+	#import <UIKit+AFNetworking.h>
 #endif
 
 
@@ -44,11 +52,6 @@
 
 #endif
 
-
-#if USE_AFNetworking
-	#import <AFNetworking.h>
-	#import <UIKit+AFNetworking.h>
-#endif
 
 #if USE_UAAppReviewManager
 	#import <UAAppReviewManager.h>
