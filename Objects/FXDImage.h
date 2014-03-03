f7a0f20680d82a9bf8848d7a96ef6d2519fc32c6
petershine <petershine@fxceed.com>
2014-03-03 23:14:21 +0900

    SIMPLIFIED importing

--- Objects/FXDImage.h
+++ Objects/FXDImage.h
@@ -47,15 +47,3 @@
 
 @end
 
-
-@interface UIImage (ImageEffects)
-
-- (UIImage *)applyLightEffect;
-- (UIImage *)applyExtraLightEffect;
-- (UIImage *)applyDarkEffect;
-- (UIImage *)applyTintEffectWithColor:(UIColor *)tintColor;
-
-- (UIImage *)applyBlurWithRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage;
-
-@end
-
