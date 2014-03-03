5a87a2a9f6c51071b6bbb5fd4492f22d5b08beb6
petershine <petershine@fxceed.com>
2014-03-03 11:11:12 +0900

    ADDED effect for uiimage

--- Objects/FXDImage.h
+++ Objects/FXDImage.h
@@ -46,3 +46,16 @@
 - (CGSize)deviceScaledSize;
 
 @end
+
+
+@interface UIImage (ImageEffects)
+
+- (UIImage *)applyLightEffect;
+- (UIImage *)applyExtraLightEffect;
+- (UIImage *)applyDarkEffect;
+- (UIImage *)applyTintEffectWithColor:(UIColor *)tintColor;
+
+- (UIImage *)applyBlurWithRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage;
+
+@end
+
