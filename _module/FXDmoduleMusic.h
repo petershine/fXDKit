e70a76a7ed28426828db178f76a5cc926ccf61e1
petershine <petershine@fxceed.com>
2014-07-14 23:49:02 +0900

    IMPLEMENTED musicModule

--- _module/FXDmoduleMusic.h
+++ _module/FXDmoduleMusic.h
@@ -1,10 +1,24 @@
 
-#warning //TODO: Implement separate module for musicPlayer
+
 #import "FXDKit.h"
 
 @import MediaPlayer;
 
 
+@interface MPMusicPlayerController (ForLowerVersion)
++ (instancetype)deviceMusicPlayer;
+@end
+
+
 @interface FXDmoduleMusic : FXDsuperModule
+
+@property (nonatomic) MPMusicPlaybackState playbackState;
+@property (nonatomic) MPMediaItem *nowPlayingItem;
+
+
+- (void)startObservingMusicPlayerNotifications;
+
+- (void)observedMPMusicPlayerControllerPlaybackStateDidChange:(NSNotification*)notification;
+- (void)observedMPMusicPlayerControllerNowPlayingItemDidChange:(NSNotification*)notification;
 
 @end
--- _module/FXDmoduleMusic.m
+++ _module/FXDmoduleMusic.m
@@ -2,32 +2,70 @@
 
 #import "FXDmoduleMusic.h"
 
+@implementation MPMusicPlayerController (ForLowerVersion)
++ (instancetype)deviceMusicPlayer {
+	MPMusicPlayerController *musicPlayer = nil;
+
+	if (SYSTEM_VERSION_sameOrHigher(iosVersion8)) {
+		musicPlayer = [MPMusicPlayerController systemMusicPlayer];
+	}
+	else {
+		musicPlayer = [MPMusicPlayerController iPodMusicPlayer];
+	}
+
+	return musicPlayer;
+}
+@end
+
+
 @implementation FXDmoduleMusic
 
 #pragma mark - Memory management
-- (void)dealloc {	FXDLog_DEFAULT;
+- (void)dealloc {
+	MPMusicPlayerController *musicPlayer = [MPMusicPlayerController deviceMusicPlayer];
+	[musicPlayer endGeneratingPlaybackNotifications];
 }
 
 
 #pragma mark - Initialization
-- (instancetype)init {
-	self = [super init];
-
-	if (self) {
-		
-	}
-
-	return self;
-}
-
 
 #pragma mark - Property overriding
 
 #pragma mark - Method overriding
 
 #pragma mark - Public
+- (void)startObservingMusicPlayerNotifications {	FXDLog_DEFAULT;
+	MPMusicPlayerController *musicPlayer = [MPMusicPlayerController deviceMusicPlayer];
+
+	NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+
+	[notificationCenter
+	 addObserver:self
+	 selector:@selector(observedMPMusicPlayerControllerPlaybackStateDidChange:)
+	 name:MPMusicPlayerControllerPlaybackStateDidChangeNotification
+	 object:musicPlayer];
+
+	[notificationCenter
+	 addObserver:self
+	 selector:@selector(observedMPMusicPlayerControllerNowPlayingItemDidChange:)
+	 name:MPMusicPlayerControllerNowPlayingItemDidChangeNotification
+	 object:musicPlayer];
+
+	[musicPlayer beginGeneratingPlaybackNotifications];
+}
 
 #pragma mark - Observer
+- (void)observedMPMusicPlayerControllerPlaybackStateDidChange:(NSNotification*)notification {
+
+	MPMusicPlayerController *musicPlayer = [MPMusicPlayerController deviceMusicPlayer];
+	self.playbackState = musicPlayer.playbackState;
+}
+
+- (void)observedMPMusicPlayerControllerNowPlayingItemDidChange:(NSNotification*)notification {
+
+	MPMusicPlayerController *musicPlayer = [MPMusicPlayerController deviceMusicPlayer];
+	self.nowPlayingItem = musicPlayer.nowPlayingItem;
+}
 
 #pragma mark - Delegate
 
