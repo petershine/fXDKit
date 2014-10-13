27d8af71b80ff54f44fc4683b4173a815be133a2
petershine <petershine@fxceed.com>
2014-10-13 23:04:51 +0900

    REMOVED unnecessary file

--- Headers/FXDimportAdopted.h
+++ Headers/FXDimportAdopted.h
@@ -55,9 +55,6 @@
 	@interface UIDevice (MultimediaFrameworks)
 	- (CGAffineTransform)affineTransformForOrientation:(UIDeviceOrientation)deviceOrientation forPosition:(AVCaptureDevicePosition)cameraPosition;
 	@end
-
-	#import "FXDMediaItem.h"
-
 #endif
 
 
