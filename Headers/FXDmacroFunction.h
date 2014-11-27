29b0cd6934571f377b898857957e495d00a21c9e
petershine <petershine@fxceed.com>
2014-11-27 22:28:35 +0900

    ADDED transform macros

--- Headers/FXDmacroFunction.h
+++ Headers/FXDmacroFunction.h
@@ -67,4 +67,59 @@
 #define radianAngleForDegree(degree)	(degree*M_PI/180.0)
 
 
+#define degreesToRadian(x) (M_PI * x / 180.0)
+#define radiansToDegrees(x) (180.0 * x / M_PI)
+
+static inline CGAffineTransform CGAffineTransformMakeShear(CGFloat shearX, CGFloat shearY) {
+	return CGAffineTransformMake(1.f, shearY, shearX, 1.f, 0.f, 0.f);
+}
+
+static inline CGAffineTransform CGAffineTransformShear(CGAffineTransform transform, CGFloat shearX, CGFloat shearY) {
+	CGAffineTransform sheared = CGAffineTransformMakeShear(shearX, shearY);
+	return CGAffineTransformConcat(transform, sheared);
+}
+
+static inline CGFloat CGAffineTransformGetDeltaX(CGAffineTransform transform) {
+	return transform.tx;
+};
+
+static inline CGFloat CGAffineTransformGetDeltaY(CGAffineTransform transform) {
+	return transform.ty;
+};
+
+static inline CGFloat CGAffineTransformGetScaleX(CGAffineTransform transform) {
+	return sqrtf( (transform.a * transform.a) + (transform.c * transform.c) );
+};
+
+static inline CGFloat CGAffineTransformGetScaleY(CGAffineTransform transform) {
+	return sqrtf( (transform.b * transform.b) + (transform.d * transform.d) );
+};
+
+static inline CGFloat CGAffineTransformGetShearX(CGAffineTransform transform) {
+	return transform.b;
+};
+
+static inline CGFloat CGAffineTransformGetShearY(CGAffineTransform transform) {
+	return transform.c;
+};
+
+static inline CGFloat CGPointAngleBetweenPoints(CGPoint first, CGPoint second) {
+	CGFloat dy = second.y - first.y;
+	CGFloat dx = second.x - first.x;
+	return atan2f(dy, dx);
+}
+
+static inline CGFloat CGAffineTransformGetRotation(CGAffineTransform transform) {
+	// No exact way to get rotation out without knowing order of all previous operations
+	// So, we'll cheat. We'll apply the transformation to two points and then determine the
+	// angle betwen those two points
+
+	CGPoint testPoint1 = CGPointMake(-100.f, 0.f);
+	CGPoint testPoint2 = CGPointMake(100.f, 0.f);
+	CGPoint transformed1 = CGPointApplyAffineTransform(testPoint1, transform);
+	CGPoint transformed2 = CGPointApplyAffineTransform(testPoint2, transform);
+	return CGPointAngleBetweenPoints(transformed1, transformed2);
+}
+
+
 #endif
