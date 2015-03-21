afaf3e3e2befc2c372537ef5c89b5724309f6758
petershine <petershine@fxceed.com>
2015-03-21 12:54:22 +0900

    UPDATED logging

--- Headers/FXDimportCore.h
+++ Headers/FXDimportCore.h
@@ -1,10 +1,4 @@
-//
-//  FXDimportCore.h
-//  FXDFramework
-//
-//  Created by petershine on 2/14/15.
-//  Copyright (c) 2015 fXceed. All rights reserved.
-//
+
 
 #ifndef FXDFramework_FXDimportCore_h
 #define FXDFramework_FXDimportCore_h
