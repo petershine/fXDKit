72d0a5553a5c579c30e12c1e76dcb8548c4bede4
Peter SHIN <petershine@fxceed.com>
2017-06-12 21:34:07 -0400

    Update FXDmoduleCoredata.m

--- _module/FXDmoduleCoredata.m
+++ _module/FXDmoduleCoredata.m
@@ -35,7 +35,6 @@
 - (NSString*)ubiquitousContentName {
 	if (_ubiquitousContentName == nil) {
 		_ubiquitousContentName = [self.coredataName stringByReplacingOccurrencesOfString:@"." withString:@"_"];
-		//-***REMOVED***
 
 		FXDLog_OVERRIDE;
 		FXDLogObject(_ubiquitousContentName);
