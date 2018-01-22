a21c2b88ed98d0feab9a708094f67f863d478e68
Peter SHIN <petershine@fxceed.com>
2018-01-21 19:42:11 -0500

    FIXED Twitter importing

--- Headers/FXDimportAdopted.h
+++ Headers/FXDimportAdopted.h
@@ -47,7 +47,7 @@
 	#import <FBSDKCoreKit/FBSDKCoreKit.h>
 	#import <FBSDKLoginKit/FBSDKLoginKit.h>
 	#import <FBSDKShareKit/FBSDKShareKit.h>
-	#import <TwitterKit/TwitterKit.h>
+	#import <TwitterKit/TWTRKit.h>
 #endif
 
 
