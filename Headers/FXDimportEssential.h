8d564b464f50a32c70d3aaa95eb1ecdc7604b90b
Peter SHIN <petershine@fxceed.com>
2018-04-03 21:54:50 -0400

    SIMPLIFIED protocol

--- Headers/FXDimportEssential.h
+++ Headers/FXDimportEssential.h
@@ -32,16 +32,6 @@ typedef void (^FXDcallbackFinish)(SEL caller, BOOL didFinish, id responseObj);
 @protocol FXDprotocolShared <NSObject>
 @required
 + (instancetype)sharedInstance;
-
-@optional
-- (void)observedUIApplicationDidEnterBackground:(NSNotification*)notification;
-- (void)observedUIApplicationDidBecomeActive:(NSNotification*)notification;
-- (void)observedUIApplicationWillTerminate:(NSNotification*)notification;
-
-- (void)observedUIApplicationDidReceiveMemoryWarning:(NSNotification*)notification;
-- (void)observedUIDeviceBatteryLevelDidChange:(NSNotification*)notification;
-
-- (void)observedUIDeviceOrientationDidChange:(NSNotification*)notification;
 @end
 
 
