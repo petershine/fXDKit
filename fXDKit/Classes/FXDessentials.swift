941b6402a7fdd361dddf7ad4e45a26336bce8336
Peter SHIN <petershine@fxceed.com>
2018-05-18 13:52:26 -0400

    rearranged

--- fXDKit/Classes/FXDessentials.swift
+++ fXDKit/Classes/FXDessentials.swift
@@ -1,33 +1,9 @@
 
 
-import os.log
 
 
 //MARK: Closures
 public typealias FXDcallback = (_ result: Bool, _ object: Any?) -> Void
-
-
-//MARK: Logging
-func FXDLog(_ items: Any..., separator: String = " ", terminator: String = "\n") {
-	#if ForDEVELOPER
-	//FIXME: How to use os_log for general cases?
-		debugPrint(items, separator: separator, terminator: terminator)
-	#endif
-}
-
-func FXDLog_Func(_ filename: String = #file, function: String = #function) {
-	#if ForDEVELOPER
-	os_log(" ")
-	os_log("[%@ %@]", (filename as NSString).lastPathComponent, function)
-	#endif
-}
-
-func FXDLog_SEPARATE(_ filename: String = #file, function: String = #function) {
-	#if ForDEVELOPER
-	os_log(" ")
-	os_log("\n\n[%@ %@]", (filename as NSString).lastPathComponent, function)
-	#endif
-}
 
 
 //MARK: Protocols
@@ -39,4 +15,29 @@ func FXDLog_SEPARATE(_ filename: String = #file, function: String = #function) {
 
 	func observedUIDeviceBatteryLevelDidChange(_ notification: NSNotification)
 	func observedUIDeviceOrientationDidChange(_ notification: NSNotification)
+}
+
+
+//MARK: Logging
+import os.log
+
+func FXDLog(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+    #if ForDEVELOPER
+    //FIXME: How to use os_log for general cases?
+    debugPrint(items, separator: separator, terminator: terminator)
+    #endif
+}
+
+func FXDLog_Func(_ filename: String = #file, function: String = #function) {
+    #if ForDEVELOPER
+    os_log(" ")
+    os_log("[%@ %@]", (filename as NSString).lastPathComponent, function)
+    #endif
+}
+
+func FXDLog_SEPARATE(_ filename: String = #file, function: String = #function) {
+    #if ForDEVELOPER
+    os_log(" ")
+    os_log("\n\n[%@ %@]", (filename as NSString).lastPathComponent, function)
+    #endif
 }
