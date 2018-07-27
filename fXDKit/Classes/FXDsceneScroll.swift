0a6f6f1323e07590f371ef49b8520e7416459da6
Peter SHIN <petershine@fxceed.com>
2018-07-27 12:08:32 -0400

    REARRANGED to develop reusable protocol

--- fXDKit/Classes/FXDsceneScroll.swift
+++ fXDKit/Classes/FXDsceneScroll.swift
@@ -1,30 +1,39 @@
 
 
 protocol FXDsceneWithScroll {
+	var didStartAutoScrollingToTop: Bool { get set }
+
+	var shouldDismissingByPullingDown: Bool { get set }
+	var didStartDismissingByPullingDown: Bool { get set }
+
+	var offsetYdismissingController: CGFloat { get set }
+
+	var cellOperationQueue: OperationQueue? { get }
+	var cellOperationDictionary: NSMutableDictionary? { get }
+
 	func dismissByPullingDown(_ scrollView: UIScrollView?)
 }
 
 
-let SCALE_SCENE_DISMISSING_OFFSET: Float = 0.275
+public let SCALE_SCENE_DISMISSING_OFFSET: Float = 0.275
 
-open class FXDsceneScroll: UIViewController {
+open class FXDsceneScroll: UIViewController, FXDsceneWithScroll {
 
 	open var didStartAutoScrollingToTop: Bool = false
 
 	open var shouldDismissingByPullingDown: Bool = false
 	open var didStartDismissingByPullingDown: Bool = false
 
-	var offsetYdismissingController: CGFloat = 0.0
-
+	open var offsetYdismissingController: CGFloat = 0.0
 
 	open lazy var cellOperationQueue: OperationQueue? = {
 		return OperationQueue.newSerialQueue(withName: String(describing: self))
 	}()
-
 	open lazy var cellOperationDictionary: NSMutableDictionary? = {
 		return NSMutableDictionary.init()
 	}()
 
+	
 	@IBOutlet open var mainScrollview: UIScrollView?
 
 
@@ -45,14 +54,15 @@ open class FXDsceneScroll: UIViewController {
 	}
 }
 
-extension FXDsceneScroll: FXDsceneWithScroll {
+extension FXDsceneScroll {
 	@objc open func dismissByPullingDown(_ scrollView: UIScrollView?) {
 		fxd_log_func()
 	}
 }
 
+
 extension FXDsceneScroll: UIScrollViewDelegate {
-	open func scrollViewDidScroll(_ scrollView: UIScrollView) {
+	@objc open func scrollViewDidScroll(_ scrollView: UIScrollView) {
 		guard shouldDismissingByPullingDown else {
 			return
 		}
@@ -64,13 +74,13 @@ extension FXDsceneScroll: UIScrollViewDelegate {
 		}
 	}
 
-	open func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
+	@objc open func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
 		didStartAutoScrollingToTop = scrollView.scrollsToTop
 
 		return scrollView.scrollsToTop
 	}
 
-	open func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
+	@objc open func scrollViewDidScrollToTop(_ scrollView: UIScrollView) {
 		didStartAutoScrollingToTop = false
 	}
 }
