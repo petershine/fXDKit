5fc6f2bc4cba8e7c65d3bd978307dd36feec48ec
Peter SHIN <petershine@fxceed.com>
2018-08-08 00:31:18 -0400

    TRANSLATED FXDWindow, renamed to FXDviewInformation

--- fXDKit/Classes/FXDWindow.swift
+++ fXDKit/Classes/FXDWindow.swift
@@ -18,75 +18,84 @@ extension UIWindow {
 
 		return newWindow
 	}
-
-    @objc public func configure(rootScene: UIViewController?, shouldAnimate: Bool, willBecomeBlock: () -> Void, didBecomeBlock: () -> Void, finishCallback: FXDcallback) {    fxd_log()
-
-        /*
-        //MARK: fade in and replace rootViewController. DO NOT USE addChildViewController
-        if (shouldAnimate == NO) {
-            if (willBecomeBlock) {
-                willBecomeBlock();
-            }
-
-            self.rootViewController = rootScene;
-
-            if (didBecomeBlock) {
-                didBecomeBlock();
-            }
-
-            if (finishCallback) {
-                finishCallback(_cmd, YES, nil);
-            }
-            return;
-        }
+}
 
 
-        UIViewController *previousRootScene = self.rootViewController;
+open class FXDviewInformation: UIView {
+	@IBOutlet weak var indicatorActivity: UIActivityIndicatorView!
+	@IBOutlet weak var labelTitle: UILabel!
+	@IBOutlet weak var labelMessage_0: UILabel!
+	@IBOutlet weak var labelMessage_1: UILabel!
+	@IBOutlet weak var sliderProgress: UISlider!
+	@IBOutlet weak var indicatorProgress: UIProgressView!
+}
 
-        if (willBecomeBlock) {
-            willBecomeBlock();
-        }
+extension UIWindow {
+	public weak var informationSubview: FXDviewInformation? {
+		var found: FXDviewInformation? = nil
+		for subview in subviews {
+			if let informationSubview = subview as? FXDviewInformation {
+				found = informationSubview
+				break
+			}
+		}
+		return found
+	}
 
-        self.rootViewController = rootScene;
+	@objc public func showInformation() {
+		let overlay = informationSubview
+		guard overlay == nil else {
+			return
+		}
 
-        [self addSubview:previousRootScene.view];
+		guard let information = FXDviewInformation.view(fromNibName: nil, owner: nil) else {
+			return
+		}
 
+		var modifiedFrame = information.frame
+		modifiedFrame.size = frame.size
+		information.frame = modifiedFrame
 
-        if (didBecomeBlock) {
-            didBecomeBlock();
-        }
+		information.alpha = 0.0
+		information.isHidden = false
 
-        if ([previousRootScene isKindOfClass:NSClassFromString(@"FXDsceneLaunching")]) {
+		addSubview(information)
+		bringSubview(toFront: information)
 
-            [previousRootScene
-                dismissFadingOutWithCallback:^(BOOL finished, id _Nullable responseObj) {
-                FXDLogBOOL(finished);
+		UIView
+			.animate(withDuration: DURATION_ANIMATION,
+					 animations: {
+						information.alpha = 1.0
+			})
+	}
 
-                [previousRootScene.view removeFromSuperview];
+	@objc public func hideInformation() {
+		guard let information = informationSubview else {
+			return
+		}
 
-                if (finishCallback) {
-                finishCallback(_cmd, finished, responseObj);
-                }
-                }];
+		UIView
+			.animate(withDuration: DURATION_ANIMATION,
+					 animations: {
+						information.alpha = 0.0
+			}) {
+				(finished) in
 
-            return;
-        }
+				information.removeFromSuperview()
+		}
+	}
 
+	@objc public func showInformationView(afterDelay: TimeInterval) {
+		DispatchQueue.main.async {
+			NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(self.showInformation), object: nil)
+			self.perform(#selector(self.showInformation), with: nil, afterDelay: afterDelay, inModes: [RunLoopMode.commonModes])
+		}
+	}
 
-        [UIView
-            animateWithDuration:durationOneSecond
-            delay:0.0
-            options:UIViewAnimationOptionCurveEaseIn
-            animations:^{
-            previousRootScene.view.alpha = 0.0;
-            }
-            completion:^(BOOL didFinish) {
-
-            [previousRootScene.view removeFromSuperview];
-
-            if (finishCallback) {
-            finishCallback(_cmd, YES, nil);
-            }
-            }];*/
-    }
+	@objc public func hideInformationView(afterDelay: TimeInterval) {
+		DispatchQueue.main.async {
+			NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(self.hideInformation), object: nil)
+			self.perform(#selector(self.hideInformation), with: nil, afterDelay: afterDelay, inModes: [RunLoopMode.commonModes])
+		}
+	}
 }
