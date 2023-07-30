a432ad8c0c12494def259201e419dce10dcc6479
petershine <petershine@fxceed.com>
2023-07-30 15:10:41 -0400

    [FXDButton] swiftified

---
+++ Sources/fXDKit/Classes/FXDButton.swift
@@ -0,0 +1,32 @@
+import fXDObjC
+
+
+extension UIButton {
+	public func replaceImageWithResizableImage(withCapInsets capInsets: UIEdgeInsets) {
+
+		[UIControl.State.normal,
+		 UIControl.State.highlighted,
+		 UIControl.State.selected,
+		 UIControl.State.disabled].forEach({
+			controlState in
+
+			if let image = self.image(for: controlState) {
+				self.setImage(image.resizableImage(withCapInsets: capInsets), for: controlState)
+			}
+		})
+	}
+
+	public func replaceBackgroundImageWithResizableImage(withCapInsets capInsets: UIEdgeInsets) {
+
+		[UIControl.State.normal,
+		 UIControl.State.highlighted,
+		 UIControl.State.selected,
+		 UIControl.State.disabled].forEach({
+			controlState in
+
+			if let image = self.backgroundImage(for: controlState) {
+				self.setBackgroundImage(image.resizableImage(withCapInsets: capInsets), for: controlState)
+			}
+		})
+	}
+}
--- Sources/fXDKit/Classes/_ObjC/View/FXDButton.h
+++
@@ -1,9 +0,0 @@
-
-#import <fXDObjC/FXDimportEssential.h>
-
-
-@interface UIButton (Essential)
-- (void)replaceImageWithResizableImageWithCapInsets:(UIEdgeInsets)capInsets;
-- (void)replaceBackgroundImageWithResizableImageWithCapInsets:(UIEdgeInsets)capInsets;
-
-@end
