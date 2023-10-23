02d548d09415af4e4df7dd3a58e73d1511b1835b
petershine <petershine@fxceed.com>
2023-10-23 19:47:18 -0400

    [deprecated] since iOS 16

--- Sources/fXDKit/Classes/FXDResponder.swift
+++ Sources/fXDKit/Classes/FXDResponder.swift
@@ -13,18 +13,15 @@ extension UIResponder {
 
 		let url: URL? = launchOptions![.url] as? URL
 		let sourceApplication = launchOptions![.sourceApplication]
-		let annotation = launchOptions![.annotation]
 
 		guard (url != nil
-			|| sourceApplication != nil
-			|| annotation != nil) else {
+			|| sourceApplication != nil) else {
 				return
 		}
 
 
 		let openOptions: [UIApplication.OpenURLOptionsKey : Any]
-			= [.sourceApplication : sourceApplication!,
-			   .annotation : annotation!]
+			= [.sourceApplication : sourceApplication!]
 		
 		if let appDelegate = self as? UIApplicationDelegate {
 			_ = appDelegate.application!(application, open: url!, options: openOptions)
@@ -41,12 +38,10 @@ extension UIResponder {
 
 		let url = launchOptions![.url]
 		let sourceApplication = launchOptions![.sourceApplication]
-		let annotation = launchOptions![.annotation]
 		let remoteNotification = launchOptions![.remoteNotification]
 
 		guard (url != nil
 			|| sourceApplication != nil
-			|| annotation != nil
 			|| remoteNotification != nil) else {
 				return false
 		}
