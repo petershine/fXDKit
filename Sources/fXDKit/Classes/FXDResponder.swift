6cb30a46d6a73613fcbf2fc1e86dfd5e3237a66b
petershine <petershine@fxceed.com>
2023-10-24 18:29:42 -0400

    [FXDResponder] refactored, to use sceneDelegate as the main scene state (what has been application state) responder

--- Sources/fXDKit/Classes/FXDResponder.swift
+++ Sources/fXDKit/Classes/FXDResponder.swift
@@ -1,5 +1,6 @@
 
 
+import SwiftUI
 import Foundation
 
 
@@ -51,17 +52,103 @@ extension UIResponder {
 }
 
 
-public protocol FXDAppDelegate: UIApplicationDelegate {
-	var window: UIWindow? { get set }
+public protocol FXDAppDelegateProtocols: UIApplicationDelegate, ObservableObject {
+	var sceneDelegateClass: AnyClass { get }
+}
 
+open class FXDAppDelegate: UIResponder, FXDAppDelegateProtocols {
+	open var sceneDelegateClass: AnyClass = FXDSceneDelegate.self
+
+	override init() {
+		super.init()
+		fxdPrint("\(#function) \(self)")
+	}
+
+
+	open func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		fxdPrint("\(#function) \(application) \(String(describing: launchOptions))")
+		return true
+	}
+
+	open func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		fxdPrint("\(#function) \(application) \(String(describing: connectingSceneSession))")
+
+		let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+		sceneConfig.delegateClass = self.sceneDelegateClass
+		return sceneConfig
+	}
+
+	open func applicationWillTerminate(_ application: UIApplication) {
+		fxdPrint("\(#function) \(application) state: \(application.applicationState)")
+	}
+}
+
+
+public protocol FXDSceneDelegateProtocols: UIWindowSceneDelegate, ObservableObject {
 	var isAppLaunching: Bool { get set }
 	var didFinishLaunching: Bool { get set }
 
-	func applicationFirstTimeBecameActiveAtLaunch(_ application: UIApplication)
+	func sceneFirstTimeBecameActiveAtLaunch(_ scene: UIScene)
 }
 
-extension FXDAppDelegate {
-	public func applicationFirstTimeBecameActiveAtLaunch(_ application: UIApplication) {
+open class FXDSceneDelegate: UIResponder, FXDSceneDelegateProtocols {
+	@Published open var isAppLaunching: Bool = false
+	@Published open var didFinishLaunching: Bool = false
+
+	lazy open var launchingView: FXDswiftuiLaunching = {
+		return FXDswiftuiLaunching(backgroundImagename: nil, foregroundImagename: nil)
+	}()
+
+
+	override init() {
+		super.init()
+		fxdPrint("\(#function) \(self)")
+	}
+
+	open func sceneDidBecomeActive(_ scene: UIScene) {
+		guard isAppLaunching == false else {
+			return
+		}
+
+
+		guard didFinishLaunching else {
+			sceneFirstTimeBecameActiveAtLaunch(scene)
+			return
+		}
+
+
+		fxdPrint("\(#function) \(scene) isAppLaunching: \(String(describing: isAppLaunching)) didFinishLaunching: \(String(describing: didFinishLaunching))")
+	}
+
+	open func sceneWillResignActive(_ scene: UIScene) {
+		fxdPrint("\(#function) \(scene)")
+	}
+
+	open func sceneWillEnterForeground(_ scene: UIScene) {
+		fxdPrint("\(#function) \(scene)")
+	}
+
+	open func sceneDidEnterBackground(_ scene: UIScene) {
+		fxdPrint("\(#function) \(scene)")
+	}
+
+	open func sceneFirstTimeBecameActiveAtLaunch(_ scene: UIScene) {
 		fxd_overridable()
+
+		fxdPrint("\(#function) \(scene) isAppLaunching: \(String(describing: isAppLaunching))")
+
+		isAppLaunching = true
+
+		let sleepingInterval = 1.0
+		Task {
+			do {
+				try await Task.sleep(nanoseconds: UInt64((sleepingInterval * 1_000_000_000).rounded()))
+			}
+
+			didFinishLaunching = true
+			isAppLaunching = false
+
+			fxdPrint("\(#function) \(scene) sleepingInterval: \(sleepingInterval) isAppLaunching: \(String(describing: isAppLaunching)) didFinishLaunching: \(String(describing: didFinishLaunching))")
+		}
 	}
 }
