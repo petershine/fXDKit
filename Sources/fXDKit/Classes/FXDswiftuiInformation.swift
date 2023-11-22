548280183a15e65044d2f72b2dd250d3efceb75d
petershine <petershine@fxceed.com>
2023-11-21 19:27:35 -0500

    [FXDswiftuiInformation] added, "shouldIgnoreUserInteraction"; renamed

--- Sources/fXDKit/Classes/FXDswiftuiInformation.swift
+++ Sources/fXDKit/Classes/FXDswiftuiInformation.swift
@@ -3,21 +3,36 @@
 import SwiftUI
 
 
-open class FXDobservableInformation: NSObject, ObservableObject {
+open class FXDconfigurationInformation: NSObject, ObservableObject {
 	@Published var overlayColor: UIColor? = nil
+	@Published var shouldIgnoreUserInteraction: Bool
 
 	@Published var informationTitle: String
 	@Published var message_0: String
 	@Published var message_1: String
 
 	@Published var sliderValue: Float
+	@Published var sliderTint: Color? = nil
 
-	public init(overlayColor: UIColor? = nil, informationTitle: String? = nil, message_0: String? = nil, message_1: String? = nil, sliderValue: Float? = nil) {
+	public init(overlayColor: UIColor? = nil, 
+				shouldIgnoreUserInteraction: Bool? = false,
+		
+				informationTitle: String? = nil,
+				message_0: String? = nil,
+				message_1: String? = nil,
+				
+				sliderValue: Float? = nil,
+				sliderTint: Color? = nil) {
+
 		self.overlayColor = overlayColor
+		self.shouldIgnoreUserInteraction = shouldIgnoreUserInteraction ?? false
+		
 		self.informationTitle = informationTitle ?? "TITLE"
 		self.message_0 = message_0 ?? "MESSAGE 0"
 		self.message_1 = message_1 ?? "MESSAGE 1"
+		
 		self.sliderValue = sliderValue ?? 0.5
+		self.sliderTint = sliderTint ?? Color(uiColor: .systemBlue)
 	}
 }
 
@@ -25,37 +40,41 @@ public struct FXDswiftuiInformation: View {
 	@Environment(\.dismiss) var dismiss
 	@Environment(\.colorScheme) var colorScheme
 	
-	@ObservedObject var information: FXDobservableInformation
+	@ObservedObject var configuration: FXDconfigurationInformation
 
 
-	public init(information: FXDobservableInformation = FXDobservableInformation()) {
-		self.information = information
+	public init(information: FXDconfigurationInformation = FXDconfigurationInformation()) {
+		self.configuration = information
 	}
 
     public var body: some View {
 		VStack {
-			Text(information.informationTitle)
+			Text(configuration.informationTitle)
 				.font(.title)
 				.fontWeight(.bold)
 
-			Text(information.message_0)
+			Text(configuration.message_0)
 
 			ProgressView()
 				.controlSize(.large)
+				.frame(alignment: .center)
 				.padding()
 
-			Slider(value: $information.sliderValue)
-			
-			Text(information.message_1)
+			Slider(value: $configuration.sliderValue)
+				.tint(configuration.sliderTint)
+				.allowsHitTesting(false)
+
+			Text(configuration.message_1)
 		}
 		.ignoresSafeArea(.all)
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
 		.padding([.leading, .trailing])
 		.contentShape(Rectangle())
 		.presentationBackground(
-			Color(uiColor: information.overlayColor ?? (colorScheme == .dark ? .black : .white))
+			Color(uiColor: configuration.overlayColor ?? (colorScheme == .dark ? .black : .white))
 				.opacity(0.75)
 		)
+		.allowsHitTesting(!configuration.shouldIgnoreUserInteraction)
 		.onTapGesture {
 			dismiss()
 		}
