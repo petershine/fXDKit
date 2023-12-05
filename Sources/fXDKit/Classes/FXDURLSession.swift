ae64bbf347cdefea88dea13f4dd19f9966d2c748
petershine <petershine@fxceed.com>
2023-12-05 16:00:35 -0500

    [FXDURLSession] updated for more generic usage

--- Sources/fXDKit/Classes/FXDURLSession.swift
+++ Sources/fXDKit/Classes/FXDURLSession.swift
@@ -4,48 +4,67 @@ import Foundation
 
 
 extension URLSession {
-	public func cancellableRandomImageRetrieving(imageURL: URL, asyncOperation: BlockOperation?) -> UIImage? {
-
+	public func synchronousURLRequest(urlRequest: URLRequest, asyncOperation: BlockOperation?, synchronousDataHandling:((Data?)->Void)?) {
 		assert((Thread.isMainThread == false || asyncOperation == nil), "\(#function) : Thread.isMainThread : \(Thread.isMainThread), asyncOperation == nil : \(asyncOperation == nil)")
 		guard (Thread.isMainThread == false || asyncOperation == nil) else {
-			return nil	// while this operation is synchronous, it should be run inside non-mainThread, for data transferring and data transforming
+			return	// while this operation is synchronous, it should be run inside non-mainThread, for data transferring and data transforming, without blocking mainThread
 		}
 
 
-		var randomImageData: Data? = nil
+		var retrievedData: Data? = nil
 
 		let synchronousRequestSemaphore = DispatchSemaphore(value: 0)
-		self.dataTask(with: URLRequest(url: imageURL)) {
-			(received, response, error) in
-			randomImageData = received
+		self.dataTask(with: urlRequest) {
+			(data, response, error) in
+			retrievedData = data
+
+			if error != nil {
+				fxdPrint("[\(#function)] error: \(String(describing: error))\nresponse: \(String(describing: response))")
+			}
 
 			synchronousRequestSemaphore.signal()
 		}.resume()
 		let result = synchronousRequestSemaphore.wait(timeout: .distantFuture)
 		fxdPrint("[\(#function)] semaphore result : \(result)")
 
-		guard (asyncOperation == nil || asyncOperation!.isCancelled == false)
-			else {
-			fxdPrint("[\(#function)] cancelled during transferring: \(!(asyncOperation == nil || asyncOperation!.isCancelled == false))")
-				return nil
+		fxdPrint("[\(#function)] cancelled during transferring: \(asyncOperation?.isCancelled ?? true)")
+		fxdPrint("[\(#function)] retrievedData : \(String(describing: retrievedData))")
+
+		synchronousDataHandling?(retrievedData)
+	}
+
+
+	public func synchronousImageRequest(urlRequest: URLRequest, asyncOperation: BlockOperation?) -> UIImage? {
+		assert((Thread.isMainThread == false || asyncOperation == nil), "\(#function) : Thread.isMainThread : \(Thread.isMainThread), asyncOperation == nil : \(asyncOperation == nil)")
+		guard (Thread.isMainThread == false || asyncOperation == nil) else {
+			return nil	// while this operation is synchronous, it should be run inside non-mainThread, for data transferring and data transforming, without blocking mainThread
 		}
 
-		guard randomImageData != nil else {
-			fxdPrint("[\(#function)] randomArtData : \(String(describing: randomImageData))")
+
+		var retrievedImage: UIImage? = nil
+
+		URLSession.shared.synchronousURLRequest(
+			urlRequest: urlRequest,
+			asyncOperation: asyncOperation,
+			synchronousDataHandling: {
+				(imageData) in
+				guard imageData != nil else {
+					return
+				}
+
+				guard (asyncOperation == nil || asyncOperation!.isCancelled == false) else {
+					return
+				}
+
+				retrievedImage = UIImage(data: imageData!)
+			})
+
+		guard (asyncOperation == nil || asyncOperation!.isCancelled == false) else {
+			fxdPrint("[\(#function)] cancelled during transforming : \(asyncOperation?.isCancelled ?? true)")
 			return nil
 		}
 
-
-		let  randomImage: UIImage? = UIImage(data: randomImageData!)
-		
-		guard (asyncOperation == nil || asyncOperation!.isCancelled == false)
-			else {
-			fxdPrint("[\(#function)] cancelled during transforming : \(!(asyncOperation == nil || asyncOperation!.isCancelled == false))")
-				return nil
-		}
-
-
-		return randomImage
+		return retrievedImage
 	}
 }
 
