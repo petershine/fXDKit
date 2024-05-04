f0da7dcea4da683233d304b36b7f9d62bc32b1a4
petershine <petershine@fxceed.com>
2024-05-04 13:54:48 -0400

    [fXDObjC] started "Traditional Framework" building

---
+++ fXDObjC/fXDObjC.h
@@ -0,0 +1,13 @@
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for fXDObjC.
+FOUNDATION_EXPORT double fXDObjCVersionNumber;
+
+//! Project version string for fXDObjC.
+FOUNDATION_EXPORT const unsigned char fXDObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <fXDObjC/PublicHeader.h>
+
+
