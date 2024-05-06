ee0c9160c9e965f9e24e2db4810c74d97609a628
petershine <petershine@fxceed.com>
2024-05-05 22:38:00 -0400

    shell script for archive and create-xcframework

---
+++ fXDObjC/_create-xcframework.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#https://developer.apple.com/documentation/xcode/creating-a-multi-platform-binary-framework-bundle
+projectName="fXDObjC"
+schemeName=$projectName
+archiveName=$projectName
+frameworkName=$projectName
+folderName="xcframeworks"
+
+
+xcodebuild archive -project "$projectName".xcodeproj \
+    -scheme $schemeName \
+    -destination "generic/platform=iOS" \
+    -archivePath "$folderName/$archiveName"
+
+xcodebuild archive -project "$projectName".xcodeproj \
+    -scheme $schemeName \
+    -destination "generic/platform=iOS Simulator" \
+    -archivePath "$folderName/$archiveName-Simulator"
+
+
+rm -rf $folderName/$frameworkName.xcframework
+
+xcodebuild -create-xcframework \
+    -archive $folderName/$archiveName.xcarchive \
+    -framework $frameworkName.framework \
+    -archive $folderName/$archiveName-Simulator.xcarchive \
+    -framework $frameworkName.framework \
+    -output $folderName/$frameworkName.xcframework
