32dba20ee8d7e19237bad642925526f5557b2e8a
petershine <petershine@fxceed.com>
2024-06-26 18:39:55 -0400

    [function] double tap to change contentMode inside scrollview

--- Sources/FXDswiftuiMediaDisplay.swift
+++ Sources/FXDswiftuiMediaDisplay.swift
@@ -4,24 +4,95 @@ import SwiftUI
 
 
 public struct FXDswiftuiMediaDisplay: View {
+	@State private var displayContentMode: ContentMode = .fit
+	@State private var displaySize: CGSize?
 
-	@Binding var displayedImage: UIImage?
-	@Binding var contentMode: ContentMode
+	@Binding var displayImage: UIImage?
 
-	public init(displayedImage: UIImage?, contentMode: ContentMode) {
-		_displayedImage = Binding.constant(displayedImage)
-		_contentMode =  Binding.constant(contentMode)
+
+	public init(displayImage: UIImage?) {
+		_displayImage = Binding.constant(displayImage)
+
+		displaySize = displayImage?.size
 	}
 
 	public var body: some View {
-		Color(.black)
-			.overlay {
-				if let displayedImage {
-					Image(uiImage: displayedImage)
-						.resizable()
-						.aspectRatio(contentMode: contentMode)
+		ScrollView([.horizontal, .vertical]) {
+			if let displayImage {
+				Image(uiImage: displayImage)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(width: displaySize?.width, height: displaySize?.height, alignment: .center)
+			}
+		}
+		.ignoresSafeArea()
+		.backgroundStyle(.black)
+		.onTapGesture(count: 2) {
+			displayContentMode = (displayContentMode == .fit) ? .fill : .fit
+		}
+		.onChange(of: displayContentMode) {
+			oldValue, newValue in
+
+			updateDisplayImage(for: newValue)
+		}
+		.onChange(of: displayImage) {
+			oldValue, newValue in
+
+			updateDisplayImage(for: displayContentMode)
+		}
+		.onReceive(NotificationCenter.default.publisher(
+			for: UIDevice.orientationDidChangeNotification)) { _ in
+				DispatchQueue.main.async {
+					updateDisplayImage(for: displayContentMode)
 				}
 			}
-			.ignoresSafeArea()
+	}
+}
+
+extension FXDswiftuiMediaDisplay {
+	func updateDisplayImage(for newContentMode: ContentMode) {
+		fxdPrint(newContentMode)
+		guard let displayImage else {
+			return
+		}
+
+
+		let aspectRatio = min(displayImage.size.width, displayImage.size.height)/max(displayImage.size.width, displayImage.size.height)
+
+		let isPortraitImage = displayImage.size.width < displayImage.size.height
+		let isPortraitScreen = UIScreen.main.bounds.width < UIScreen.main.bounds.height
+
+
+		var newSize: CGSize = displaySize ?? displayImage.size
+
+		if newContentMode == .fit {
+			let minDimension = min(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+
+			if (isPortraitImage && isPortraitScreen)
+				|| (!isPortraitImage && isPortraitScreen) {
+				newSize = CGSize(width: minDimension, height: minDimension/aspectRatio)
+			}
+			else if (isPortraitImage && !isPortraitScreen)
+						|| (!isPortraitImage && !isPortraitScreen) {
+				newSize = CGSize(width: minDimension*aspectRatio, height: minDimension)
+			}
+		}
+		else {
+			let maxDimension = max(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+
+			if (isPortraitImage && isPortraitScreen)
+				|| (!isPortraitImage && isPortraitScreen) {
+				newSize = CGSize(width: maxDimension*aspectRatio, height: maxDimension)
+			}
+			else if (isPortraitImage && !isPortraitScreen)
+						|| (!isPortraitImage && !isPortraitScreen) {
+				newSize = CGSize(width: maxDimension, height: maxDimension/aspectRatio)
+			}
+		}
+
+		fxdPrint(newSize)
+		withAnimation {
+			displaySize = newSize
+		}
 	}
 }
