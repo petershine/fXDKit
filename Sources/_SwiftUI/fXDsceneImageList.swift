410994295dbbf60b7fc5eb87e43249b5e9df7c99
petershine <petershine@fxceed.com>
2024-06-30 16:33:43 -0400

    [promote] as fXDsceneImageList

---
+++ Sources/_SwiftUI/fXDsceneImageList.swift
@@ -0,0 +1,74 @@
+
+
+import SwiftUI
+
+
+public struct fXDsceneImageList: View {
+	@State var minDimension: CGFloat
+
+	@Binding var imageURLs: [URL]
+	@Binding var selectedImageURL: URL?
+
+
+	public init(minDimension: CGFloat? = nil, imageURLs: Binding<[URL]>, selectedImageURL: Binding<URL?>) {
+		self.minDimension = minDimension ?? (UIDevice.current.userInterfaceIdiom == .phone ? 80.0 : 100.0)
+
+		_imageURLs = imageURLs
+		_selectedImageURL = selectedImageURL
+	}
+
+
+	public var body: some View {
+		List(
+			imageURLs,
+			id:\.self
+		) {
+			imageURL in
+
+			LazyVStack {
+				AsyncImage(url: imageURL) { pngImage in
+					pngImage
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.overlay(content: {
+							if imageURL == selectedImageURL {
+								Rectangle()
+									.stroke(Color.white, lineWidth: 1.0)
+							}
+						})
+				}
+				placeholder: {
+				}
+				.frame(width: minDimension, height: minDimension, alignment: .center)
+				.clipShape(Rectangle())
+				.onTapGesture(perform: {
+					selectedImageURL = imageURL
+				})
+				.onLongPressGesture {
+					DispatchQueue.main.async {
+						UIActivityViewController.show(items: [imageURL])
+					}
+				}
+			}
+			.listRowBackground(Color.clear)
+			.listRowSeparator(.hidden)
+		}
+		.frame(width: minDimension)
+		.shadow(color: .black, radius: 10.0)
+		.scrollIndicators(.hidden)
+		.listStyle(.plain)
+		.listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+		.onAppear(perform: {
+			if let latest = imageURLs.first {
+				selectedImageURL = latest
+			}
+		})
+		.onChange(of: imageURLs, {
+			oldValue, newValue in
+
+			if let latest = newValue.first {
+				selectedImageURL = latest
+			}
+		})
+	}
+}
