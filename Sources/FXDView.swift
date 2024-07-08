88edaeb3a4009d8a969d93a22609e0d17c3884fa
petershine <petershine@fxceed.com>
2024-07-08 18:59:38 -0400

    [reorganized]

--- Sources/FXDView.swift
+++ Sources/FXDView.swift
@@ -138,18 +138,3 @@ extension UIView {
 		self.layer.add(blinkShadow, forKey: "shadowOpacity")
 	}
 }
-
-
-import SwiftUI
-
-extension View {
-	public func shouldHide(_ shouldHide: Bool) -> some View {
-		modifier(fXDViewModifier(shouldHide: shouldHide))
-	}
-}
-
-
-@ViewBuilder public func SPACER_FIXED(_ maxDimension: CGFloat = max(SIZE_TOUCHABLE.width, SIZE_TOUCHABLE.height)) -> some View {
-	Spacer().frame(width: maxDimension, height: maxDimension, alignment: .center)
-}
-
