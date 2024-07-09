c5e223feb1a8c8adc2215b877bd6f6b75a349c2d
petershine <petershine@fxceed.com>
2024-07-08 23:00:08 -0400

    [useful] for immediate need for ASYNC_IMAGE, like for debugging

--- Sources/_SwiftUI/fXDSwiftUI.swift
+++ Sources/_SwiftUI/fXDSwiftUI.swift
@@ -13,3 +13,19 @@ extension View {
 @ViewBuilder public func SPACER_FIXED(_ maxDimension: CGFloat = max(SIZE_TOUCHABLE.width, SIZE_TOUCHABLE.height)) -> some View {
 	Spacer().frame(width: maxDimension, height: maxDimension, alignment: .center)
 }
+
+
+@ViewBuilder public func ASYNC_IMAGE(_ imageURL: URL?) -> some View {
+	AsyncImage(
+		url: imageURL,
+		content: {
+			pngImage in
+			pngImage
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+		},
+		placeholder: {
+		})
+	.frame(width: DIMENSION_PREVIEW, height: DIMENSION_PREVIEW, alignment: .center)
+	.clipShape(Rectangle())
+}
