d92c2ec38b9305db4df282fa0f263eeabcbbd8ec
petershine <petershine@fxceed.com>
2025-04-27 13:52:38 -0400

    [button] added glowing circle as background

--- Sources/_SwiftUI/fXDbuttonSymbol.swift
+++ Sources/_SwiftUI/fXDbuttonSymbol.swift
@@ -10,7 +10,11 @@ public struct fXDbuttonSymbol: View {
 	var backgroundStyle: Color
 	var strokeStyle: Color
 	var hideShadow: Bool
-	var action: () -> Void
+    var shouldAnimate: Bool
+    var action: () -> Void
+
+    @State private var animatingOpacity: CGFloat = 1.0
+
 
 	public init(
 		_ systemName: String,
@@ -19,6 +23,7 @@ public struct fXDbuttonSymbol: View {
 		backgroundStyle: Color = .black,
 		strokeStyle: Color = .clear,
 		hideShadow: Bool = false,
+        shouldAnimate: Bool = false,
 		action: @escaping () -> Void) {
 
 			self.systemName = systemName
@@ -27,23 +32,41 @@ public struct fXDbuttonSymbol: View {
 			self.backgroundStyle = backgroundStyle
 			self.strokeStyle = strokeStyle
 			self.hideShadow = hideShadow
+            self.shouldAnimate = shouldAnimate
 			self.action = action
 	}
 	
-	public var body: some View {
-		Button(action: action) {
-			Image(systemName: systemName)
-				.resizable()
-				.aspectRatio(contentMode: .fit)
-		}
-		.modifier(
-			fXDButtonModifier(
-				touchableSize: touchableSize,
-				foregroundStyle: foregroundStyle,
-				backgroundStyle: backgroundStyle,
-				strokeStyle: strokeStyle,
-				hideShadow: hideShadow,
-				cornerRadius: 5.0)
-		)
-	}
+    public var body: some View {
+        ZStack {
+            Circle()
+                .fill(backgroundStyle)
+                .opacity(animatingOpacity)
+                .blur(radius: 8.0)
+                .frame(width: touchableSize.width, height: touchableSize.height)
+
+            Button(action: action) {
+                Image(systemName: systemName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            .modifier(
+                fXDButtonModifier(
+                    touchableSize: touchableSize,
+                    foregroundStyle: foregroundStyle,
+                    backgroundStyle: backgroundStyle,
+                    strokeStyle: strokeStyle,
+                    hideShadow: hideShadow,
+                    cornerRadius: 5.0)
+            )
+        }
+        .onAppear {
+            if shouldAnimate {
+                withAnimation(
+                    Animation.easeInOut(duration: 1.0)
+                        .repeatForever(autoreverses: true)) {
+                            animatingOpacity = 0.8
+                        }
+            }
+        }
+    }
 }
