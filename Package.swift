606b2455fb6da32f43e0813043be50c8bbdce89a
petershine <petershine@fxceed.com>
2025-09-02 21:11:44 -0400

    [firebase] 12.2.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "11.14.0"))
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "12.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
