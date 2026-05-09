f3753ab49e7a83c1de1e7de9e3a11c919622acdc
petershine <petershine@fxceed.com>
2026-05-09 01:09:04 -0400

    [Xcode 26.5]

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,9 @@ import PackageDescription
 let package = Package(
     name: "fXDKit",
     platforms: [
-        .iOS(.v18),
-        .macCatalyst(.v18),
-        .macOS(.v14)
+        .iOS(.v26),
+        .macCatalyst(.v26),
+        .macOS(.v26)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "12.12.1"))
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "12.13.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
